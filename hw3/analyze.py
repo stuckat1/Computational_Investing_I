9fcabaeeab04be8f601307f050bcc67223d66e13
stuckat1 <keweima@yahoo.com>
2014-10-15 15:10:12 -0400

    Add performance results

--- hw3/analyze.py
+++ hw3/analyze.py
@@ -9,7 +9,7 @@ Home Work #3
 
 Portfolio returns analyzer
 '''
-
+import QSTK.qstkutil.tsutil as tsu
 import QSTK.qstkutil.DataAccess as da
 
 # Third Party Imports
@@ -48,16 +48,30 @@ def read_benchmark( symbol, time_stamps):
 
     return close[symbol]
 
+def print_performance( fund, bench):
+    print "Details of the Performance of the portfolio :\n"
+
+    print "Data Range : ", fund.index[0], " to " ,fund.index[-1],"\n"
+
+    print "Sharpe Ratio of Fund : ", tsu.get_sharpe_ratio(tsu.daily(fund))[0]
+    print "Sharpe Ratio of " + benchmark +" :", tsu.get_sharpe_ratio(tsu.daily(bench))[0],"\n"
+
+    print 'Total Return of Fund :', (fund[-1] / fund[0] - 1) + 1
+    print 'Total Return of ' + benchmark + ' : ', (bench[-1] / bench[0] -1) + 1, "\n"
+
+    print 'Standard Deviation of Fund : ' + str(np.std(tsu.daily(fund.values)))
+    print 'Standard Deviation of ' + benchmark + ' : ', np.std(tsu.daily(bench.values)), "\n"
+
+    print 'Average Daily Return of Fund : ', np.mean( tsu.daily(fund.values))
+    print 'Average Daily Return of ' + benchmark + ' : ', np.mean( tsu.daily(bench.values))
+
 
 if __name__ == '__main__':
 
     fund_input_file = sys.argv[1]
     benchmark = sys.argv[2].strip().upper()
 
-    #print fund_input_file, ",", benchmark
-
     fund = read_fund( fund_input_file)
-    benchmark_prices = read_benchmark( benchmark, list( fund.index))
+    bench = read_benchmark( benchmark, list( fund.index))
 
-    print "fund = ", fund
-    print "bench = ", benchmark_prices
+    print_performance( fund, bench)
